223b0119fa6373c6bdea2c6be270dfa0d60f6b99
Joshua Getner <jgetner@gmail.com>
2024-10-07 15:19:11 -0700

    attempting to fix.

--- compiled/methods/client-search.js
+++ compiled/methods/client-search.js
@@ -1,28 +1,47 @@
 "use strict";
 
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = _default;
+
+var _objectFilledKeysCount = require("../helpers/object-filled-keys-count");
+
+var _isValidMomentObject = require("../helpers/is-valid-moment-object");
+
+var _customFilters = _interopRequireDefault(require("../filters/custom-filters"));
+
+var _moment = _interopRequireDefault(require("moment"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
 function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
-var object_filled_keys_count = require("../helpers/object-filled-keys-count");
+function _default(data, e) {
+  var _this = this;
 
-var is_valid_moment_object = require("../helpers/is-valid-moment-object");
-
-var filterByCustomFilters = require("../filters/custom-filters");
-
-module.exports = function (data, e) {
   if (e) {
-    var _query = this.query;
+    var _query = this.query || {}; // Handle input event and query update
+
+
     this.setPage(1, true);
     var name = this.getName(e.target.name);
-    var value = _typeof(e.target.value) === "object" ? e.target.value : "" + e.target.value;
+    var value = _typeof(e.target.value) === "object" ? e.target.value : String(e.target.value || ""); // Update query based on the input field
 
     if (name) {
       _query[name] = value;
     } else {
       _query = value;
+    } // Vuex or local state management (reactive handling)
+
+
+    if (this.vuex) {
+      this.commit("SET_FILTER", _query);
+    } else {
+      this.query = _query;
     }
 
-    this.vuex ? this.commit("SET_FILTER", _query) : this.query = _query;
-    this.updateState("query", _query);
+    this.updateState("query", _query); // Dispatching filter event (emit or custom event handling)
 
     if (name) {
       this.dispatch("filter", {
@@ -35,99 +54,92 @@ module.exports = function (data, e) {
     }
   }
 
-  var query = this.query;
-  var totalQueries = !query ? 0 : 1;
-  if (!this.opts) return data;
+  var query = this.query || {};
+  var totalQueries = this.opts.filterByColumn ? (0, _objectFilledKeysCount.object_filled_keys_count)(query) : 1;
+  if (!this.opts) return data; // Apply custom filters (external filters applied before default filters)
 
-  if (this.opts.filterByColumn) {
-    totalQueries = object_filled_keys_count(query);
-  }
+  data = (0, _customFilters["default"])(data, this.opts.customFilters, this.customQueries); // If no valid queries, return original data set
 
-  var value;
-  var found;
-  var currentQuery;
-  var dateFormat;
-  var filterByDate;
-  var isListFilter;
-  console.log("data before", data.length);
-  console.log("customFilters", this.opts.customFilters);
-  console.log("customQueries", this.customQueries);
-  var data = filterByCustomFilters(data, this.opts.customFilters, this.customQueries);
-  console.log("data after", data.length);
-  console.log("totalQueries", totalQueries);
-  if (!totalQueries) return data;
-  return data.filter(function (row, index) {
-    found = 0;
-    console.log("filterableColumns", this.filterableColumns);
-    this.filterableColumns.forEach(function (column) {
-      filterByDate = this.opts.dateColumns.indexOf(column) > -1 && this.opts.filterByColumn;
-      isListFilter = this.isListFilter(column) && this.opts.filterByColumn;
-      dateFormat = this.dateFormat(column);
-      value = this._getValue(row, column);
+  if (!totalQueries) return data; // Filter the data rows based on the column query filters
 
-      if (is_valid_moment_object(value) && !filterByDate) {
-        console.log("inside is_valid_moment_object");
+  return data.filter(function (row) {
+    var found = 0;
+
+    _this.filterableColumns.forEach(function (column) {
+      var value = _this._getValue(row, column); // Get the value of the current column in the row
+
+
+      var filterByDate = _this.opts.dateColumns.includes(column) && _this.opts.filterByColumn;
+
+      var isListFilter = _this.isListFilter(column) && _this.opts.filterByColumn;
+
+      var dateFormat = _this.dateFormat(column); // Handle date formatting if the column contains moment objects
+
+
+      if ((0, _isValidMomentObject.is_valid_moment_object)(value) && !filterByDate) {
         value = value.format(dateFormat);
       }
 
-      currentQuery = this.opts.filterByColumn ? query[column] : query;
-      currentQuery = setCurrentQuery(currentQuery);
-      console.log("currentQuery", currentQuery);
+      var currentQuery = _this.opts.filterByColumn ? query[column] : query;
+      currentQuery = setCurrentQuery(currentQuery); // Check for a match if a valid query exists
 
       if (currentQuery) {
-        console.log("in current query");
-        console.log("this.opts.filterAlgorithm", this.opts.filterAlgorithm, column);
-
-        if (this.opts.filterAlgorithm[column]) {
-          console.log("in filter algo");
-          if (this.opts.filterAlgorithm[column].call(this.$parent.$parent, row, this.opts.filterByColumn ? query[column] : query)) found++;
+        if (_this.opts.filterAlgorithm && _this.opts.filterAlgorithm[column]) {
+          // Use custom filter algorithm if specified
+          if (_this.opts.filterAlgorithm[column].call(_this.$parent, row, currentQuery)) {
+            found++;
+          }
         } else {
-          if (foundMatch(currentQuery, value, isListFilter)) found++;
-          return data;
+          // Default matching logic
+          if (foundMatch(currentQuery, value, isListFilter)) {
+            found++;
+          }
         }
       }
-    }.bind(this));
-    console.log(found, totalQueries);
+    }); // Return true if the number of matched queries equals or exceeds the total queries
+
+
     return found >= totalQueries;
-  }.bind(this));
-};
+  });
+} // Helper function to standardize the query for matching
+
 
 function setCurrentQuery(query) {
   if (!query) return "";
-  if (typeof query == "string") return query.toLowerCase(); // Date Range
+  if (typeof query === "string") return query.toLowerCase(); // Convert string queries to lowercase
 
-  return query;
-}
+  return query; // Return query as is (for object/array queries)
+} // Match the query to the row's value, considering list filters and text matching
+
 
 function foundMatch(query, value, isListFilter) {
-  if (["string", "number", "boolean"].indexOf(_typeof(value)) > -1) {
-    value = String(value).toLowerCase();
-  } // List Filter
+  if (["string", "number", "boolean"].includes(_typeof(value))) {
+    value = String(value).toLowerCase(); // Convert value to string and lowercase for comparison
+  } // Handle list filters (exact match required)
 
 
   if (isListFilter) {
     return value == query;
-  } //Text Filter
+  } // Handle text filters (contains matching)
 
 
   if (typeof value === "string") {
-    return value.indexOf(query) > -1;
-  } // Date range
+    return value.includes(query);
+  } // Handle date range filters
 
 
-  if (is_valid_moment_object(value)) {
-    var start = moment(query.start, "YYYY-MM-DD HH:mm:ss");
-    var end = moment(query.end, "YYYY-MM-DD HH:mm:ss");
-    return value >= start && value <= end;
+  if ((0, _isValidMomentObject.is_valid_moment_object)(value) && _typeof(query) === "object" && query.start && query.end) {
+    var start = (0, _moment["default"])(query.start, "YYYY-MM-DD HH:mm:ss");
+    var end = (0, _moment["default"])(query.end, "YYYY-MM-DD HH:mm:ss");
+    return value.isBetween(start, end, null, "[]"); // Inclusive date range comparison
+  } // Handle object comparisons (recursive check for nested properties)
+
+
+  if (_typeof(value) === "object" && !Array.isArray(value)) {
+    return Object.keys(value).some(function (key) {
+      return foundMatch(query, value[key], isListFilter);
+    });
   }
 
-  if (_typeof(value) === "object") {
-    for (var key in value) {
-      if (foundMatch(query, value[key])) return true;
-    }
-
-    return false;
-  }
-
-  return value >= start && value <= end;
+  return false; // Return false if no match found
 }
